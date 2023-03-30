75ca9031efd2402c409817180dc1724ec3e154ca
underlamafile <pavel.filonenko@urfu.me>
2023-03-30 20:39:03 +0500

    fixing errors

--- src/GraphEditor/GraphEditor/DataTypes/Meta.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GraphEditor.DataTypes
-{
-    public class Meta
-    {
-        public string? Color { get; set; } = null;
-        public string? Type { get; set; } = null;
-    }
-}
--- src/GraphEditor/GraphEditor/scripts/ReCreate Db and Migrations.cmd
+++
@@ -1,6 +0,0 @@
-cd..
-sqlcmd -i "./scripts/DropAll.sql"
-dotnet ef migrations remove
-dotnet ef migrations add Init
-dotnet ef database update
-pause
